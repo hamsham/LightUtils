5b1a1f005c4de57874431fd2cccf25fcf42e9509
hammy <mileslacey@gmail.com>
2020-12-21 22:36:43 -0800

    Ensuring CMake dependencies for third-party libraries are correctly configured.

--- enetConfig.cmake
+++ enetConfig.cmake
@@ -114,7 +114,13 @@ else()
     add_library(enet STATIC IMPORTED)
     set_target_properties(enet PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${EXTERNAL_PROJECT_PREFIX}/include)
     set_target_properties(enet PROPERTIES IMPORTED_LOCATION ${EXTERNAL_PROJECT_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}enet${CMAKE_STATIC_LIBRARY_SUFFIX})
-    add_dependencies(enet ENet)
+
+    add_custom_command(
+        OUTPUT ${EXTERNAL_PROJECT_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}enet${CMAKE_STATIC_LIBRARY_SUFFIX}
+        COMMAND ""
+        DEPENDS ENet
+    )
+
 
     set(ENET_INCLUDE_DIR "${EXTERNAL_PROJECT_PREFIX}/include")
 
