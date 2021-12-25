6ec50a5ee9de79b856acb4e54530c043ae530cb4
hammy <mileslacey@gmail.com>
2021-12-24 17:31:45 -0800

    Adding Tuple example for element iteration.

--- tests/lsutils_tuple_test.cpp
+++ tests/lsutils_tuple_test.cpp
@@ -5,9 +5,10 @@
  * Created on October 11, 2015, 6:51 PM
  */
 
-#include <utility>
-#include <string>
 #include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "lightsky/utils/Tuple.h"
 
@@ -28,6 +29,71 @@ void print_tuple(const std::string& name, const test_t& t)
         << "\n\t" << t.first_of<float>()
         << "\n\t" << t.const_element<3>()
         << '\n' << std::endl;
+}
+
+
+
+template <unsigned numArgs, unsigned currentArg, typename... ArgsType>
+struct SoaPrinter;
+
+template <typename... ArgsType, unsigned numArgs>
+struct SoaPrinter<numArgs, sizeof...(ArgsType)-1u, ArgsType...>
+{
+    void operator()(const ls::utils::Tuple<ArgsType...>& tuple)
+    {
+        const auto& vec = tuple.template const_element<sizeof...(ArgsType)-1u>();
+        for (const auto& element : vec)
+        {
+            std::cout << element << ' ';
+        }
+        std::cout << std::endl;
+    }
+};
+
+template <unsigned numArgs, unsigned currentArg, typename... ArgsType>
+struct SoaPrinter
+{
+    void operator()(const ls::utils::Tuple<ArgsType...>& tuple)
+    {
+        const auto& vec = tuple.template const_element<currentArg>();
+        for (const auto& element : vec)
+        {
+            std::cout << element << ' ';
+        }
+        std::cout << std::endl;
+
+        SoaPrinter<sizeof...(ArgsType), currentArg+1u, ArgsType...> soaPrinter;
+        soaPrinter(tuple);
+    }
+};
+
+
+
+template <typename... ArgsType>
+void print_soa(const ls::utils::Tuple<ArgsType...>& tuple) noexcept
+{
+    SoaPrinter<sizeof...(ArgsType), 0u, ArgsType...> soaPrinter;
+    soaPrinter(tuple);
+}
+
+
+
+void test_soa()
+{
+    Tuple<std::vector<int>, std::vector<float>, std::vector<std::string>> soa;
+    soa.element<0>().push_back(1);
+    soa.element<0>().push_back(2);
+    soa.element<0>().push_back(3);
+
+    soa.element<1>().push_back(4.4f);
+    soa.element<1>().push_back(5.5f);
+    soa.element<1>().push_back(6.6f);
+
+    soa.element<2>().push_back("seven");
+    soa.element<2>().push_back("eight");
+    soa.element<2>().push_back("nine");
+
+    print_soa(soa);
 }
 
 
@@ -55,6 +121,8 @@ int main() {
 
     c.element<3>() = "Goodbye Cruel World!";
     print_tuple("C", c);
+
+    test_soa();
 
     return 0;
 }
